b2f89d35a82f54387055a834778bb4988bab24ac
fernando-albsilva <fernando.albsilva@gmail.com>
2019-10-02 13:43:16 -0300

    Adicionando comentarios no arquivo main.js

--- JS/main.js
+++ JS/main.js
@@ -1,16 +1,21 @@
-
+/* Função preencher(); recebe o parametro passado pelo valor do botão clicado no html,
+    e concatena no visor o valor recdebido*/
 function preencher(valor) 
 {
     document.getElementById('visor').value+=valor;
 }
 
-
+/*Função calcular(); guarda na variavel total a expressao completa que se encontra no visor,
+    em seguida limpa a tela do visor, e após essas etapas coloca no visor o resultado da expressão
+    guardada na variavel total */
 function calcular(){
     var total=0;
     total=document.getElementById('visor').value;
     document.getElementById('visor').value=' ';
     document.getElementById('visor').value=eval(total);
 }
+
+/*Função limpar(); poe no visor um espaço em branco ' ' , com o objetivo de limpar a tela*/
 function limpar(){
     document.getElementById('visor').value=' ';
 }
